fa4568882754198d862bdbd8d03e6fd658ce8161
Jesus Carvajal <jesusfernandocarvajal2018@gmail.com>
2023-10-15 11:47:33 -0500

    Borrar personal JSON (cliente)

--- js/json/eliminarPersonal.js
+++ js/json/eliminarPersonal.js
@@ -1,32 +1,42 @@
-document.getElementById('delete1').onclick = function coca(){
-
-    alert('holaaaaa');
-    
-    // const id_personal = document.getElementById('idPersonalIns').value;
-
-    // const data_delete={
-    //     id_personal:id_personal,
-    // };
-
-    // fetch('librerias/borrar_personal.php', {
-    //     method: 'POST',
-    //     body: JSON.stringify(data_delete),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    // })
-    
-    // .then(response => response.json())
-    // .then(data => {
-    //     if(data.eliminacion == "OK"){
-    //         tablaPersonal();
-    //     }else{
-    //         alert("error al borrar el registro")
-    //     }
-    // })
-    // .catch(error => console.log(error))
-
-
-
-
+function validacionEliminar(){
+    const deseaHacerCambio = window.confirm("¿Estás seguro de que deseas eliminar este registro?");
+    return deseaHacerCambio;
 }
+
+function eliminarRegistro(personalId){
+    if(validacionEliminar() == true){
+        const data_delete={
+            idPersonal:personalId,
+        };
+    
+        fetch('librerias/borrar_personal.php', {
+            method: 'POST',
+            body: JSON.stringify(data_delete),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        
+        .then(response => response.json())
+        .then(data => {
+            if(data.eliminacion == "OK"){
+                tablaPersonal();
+            }else{
+                alert("error al borrar el registro")
+            }
+        })
+        .catch(error => console.log(error))
+    }else{
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+// }
