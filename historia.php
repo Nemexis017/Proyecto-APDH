3c6fe56b2de237bb72e7e653ddb1e83a18054cb1
Mairena <ceidyyague21@gmail.com>
2023-11-09 16:54:17 -0500

    Historia(Info-img)

--- historia.php
+++ historia.php
@@ -40,12 +40,10 @@
                         <div class="h_blog_text_inner left right">
                             <h4>Nuestra Historia</h4>
                             <p class="text-justify">
-                                La imaginación nos permite remontamos 50 años atrás en la historia del municipio de Iquira
-                                y encontrar en un recinto comunal, una reunión de siete honorables personalidades, que muy
-                                preocupados por el futuro de sus hijos y en general por la formación de los iquireños, sesionan 
-                                como primera junta pro-colegio para el municipio, pues corría el mes de octubre del año 1966 y
-                                había que pensar desde ya, dónde y cómo se organizaría el estudio de los niños que terminarían 
-                                su primaria. 
+                            El colegio experimentó cambios en su rectoría a lo largo de los años, con diferentes rectores como Jaime Izquierdo,    
+                            Martha Inés Alarcón Rojas, Luz Alba Yustres Tovar y Rosalba Yustres. Hubo fluctuaciones en el número de alumnos y docentes, 
+                            así como la implementación de proyectos educativos. En 2003, se convirtió en una Institución Educativa y se fusionó con otras escuelas. 
+                            Desde 2004, el rector ha sido Alirio Guevara Sánchez. Durante estos años se realizaron actividades como la creación de un periódico escolar y la incorporación de nuevas tecnologías.
                             </p>
                         </div>
                     </div>
@@ -63,7 +61,7 @@
                         <div class="blog_left_sidebar">
                             <article class="row blog_item">
                                 <div class="col-md-5">
-                                    <img src="img/blog/main-blog/m-blog-1.jpg" alt="" width="450px">
+                                    <img src="img/fotosHistoria/img7.jpg" alt="" width="450px">
                                 </div>
                                 <div class="col-md-7">
                                     <div class="col-md-12">
@@ -76,16 +74,13 @@
                                     <div class="blog_post">
                                         <div class="blog_details">
                                             <p>
-                                                se creó la Junta Pro- colegio de Iquira, presidida por el Presbítero José Vicente Pérez y 
-                                                los señores Miguel Cedeño, Arcesio Andrade, Floresmiro Pérez, Ignacio A. Cadena, Leonardo Córdoba, 
-                                                Carlos Julio Salazar y Jaime Perdomo.
-                                            </p>
-                                            <!-- <p>
-                                                El Colegio Municipal María Auxiliadora inició sus actividades en febrero de 1967 gracias a la gestión
-                                                del Presbítero Pérez y un grupo de Iquireños preocupados por la educación de la juventud. Lleva el nombre 
-                                                de María Auxiliadora y la tiene a ella misma como Patrona.
-                                            </p> -->
-                                        </div>
+                                            El 9 de octubre de 1966 se creó la Junta Pro- colegio de Iquira, presidida por el Presbítero José Vicente Pérez y los señores Miguel Cedeño, Arcesio Andrade, 
+                                            Floresmiro Pérez, Ignacio A. Cadena, Leonardo Córdoba, Carlos Julio Salazar y Jaime Perdomo.
+                                            El Colegio Municipal María Auxiliadora inició sus actividades en febrero de 1967 gracias a la gestión del Presbítero Pérez y un grupo de Iquireños preocupados por la educación de la juventud. 
+                                            Lleva el nombre de María Auxiliadora y la tiene a ella misma como Patrona.
+                                            
+                                            El 20 de febrero de 1967 se contaba con los grados 5º de primaria y 1º de bachillerato, bajo la rectoría del Presbítero José Vicente Pérez y las profesoras María Elena Zúñiga y Ana Silvia Pérez, 
+                                            en propiedades del municipio, antigua Escuela de Artes y Oficios, al frente de donde hoy funciona la Sede Central de la Institución Educativa María Auxiliadora.
                                     </div>
                                 </div>
                             </article>
@@ -108,9 +103,9 @@
 
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img1.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img1.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -120,9 +115,20 @@
 
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img2.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img2.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
+                                        <h4 class="mb-3">
+                                            <a href="course-details.html">Historia</a>
+                                        </h4>
+                                        </div>
+                                    </div>
+
+                                    <div class="single_course">
+                                        <div class="sufro">
+                                        <img class="img-fluid" src="img/fotosHistoria/img3.jpg" alt="" />
+                                        </div>
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -131,9 +137,9 @@
 
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img3.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img4.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -142,20 +148,9 @@
 
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img4.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img5.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
-                                        <h4 class="mb-3">
-                                            <a href="course-details.html">Historia</a>
-                                        </h4>
-                                        </div>
-                                    </div>
-
-                                    <div class="single_course">
-                                        <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img5.jpeg" alt="" />
-                                        </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -163,9 +158,9 @@
                                     </div>
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img6.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img6.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -173,19 +168,9 @@
                                     </div>
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img7.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img8.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
-                                        <h4 class="mb-3">
-                                            <a href="course-details.html">Historia</a>
-                                        </h4>
-                                        </div>
-                                    </div>
-                                    <div class="single_course">
-                                        <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img8.jpeg" alt="" />
-                                        </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -194,9 +179,9 @@
                                     
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img9.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img9.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -205,9 +190,9 @@
 
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img10.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img10.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
@@ -216,9 +201,9 @@
 
                                     <div class="single_course">
                                         <div class="course_head">
-                                        <img class="img-fluid" src="img/fotosHistoria/img11.jpeg" alt="" />
+                                        <img class="img-fluid" src="img/fotosHistoria/img11.jpg" alt="" />
                                         </div>
-                                        <div class="course_content">
+                                        <div class="course_contenthistoria">
                                         <h4 class="mb-3">
                                             <a href="course-details.html">Historia</a>
                                         </h4>
